990637125477518bb0dcaf969b3a499103b69823
Usuario <Usuario@DESKTOP-5J2QE9O>
2018-02-08 14:16:24 -0600

    Ver 1.0

---
+++ src/main/java/com/boot/test/Practica6GitApplication.java
@@ -0,0 +1,12 @@
+package com.boot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica6GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica6GitApplication.class, args);
+	}
+}
